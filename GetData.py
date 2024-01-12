f62c61267bc85e5c13deaf6e444eba50d0d2e663
Nguyen hoang Anh <hoanganhbvh2@gmail.com>
2024-01-12 15:29:44 +0700

    update

---
+++ GetData.py
@@ -0,0 +1,14 @@
+import openai
+# Đặt API key của bạn
+openai.api_key = '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'
+# trang check engine
+#https://stackoverflow.com/questions/77789886/openai-api-error-the-model-text-davinci-003-has-been-deprecated
+# Gửi yêu cầu và nhận kết quả
+response = openai.Completion.create(
+    engine="gpt-3.5-turbo-instruct",  # Chọn mô hình GPT-3
+    prompt="Chỉ chỗ cho tôi đến Bắc Từ Liêm ?",
+    max_tokens=100  # Số lượng từ tối đa trong kết quả
+)
+
+# In kết quả
+print(response.choices[0].text)
